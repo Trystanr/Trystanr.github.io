ceff07a3f087b5050927606c5abab9c5e92fd025
Trystan Rivers <trystan.b@hotmail.com>
2017-11-10 14:55:55 +0200

    Menu

--- StudentAffix/js/script.js
+++ StudentAffix/js/script.js
@@ -3,3 +3,33 @@ $("form :input").focus(function() {
 }).blur(function() {
   $("label").removeClass("labelfocus");
 });
+
+var bMenuToggle = false;
+
+$( "#nav-toggle" ).on( "click", function() {
+	if (bMenuToggle) {
+		//Close menu
+
+		bMenuToggle = false;
+		$( "#navbar" ).removeClass("nav-full");
+		$('body').off('scroll mousewheel touchmove', stopScrolling);
+
+		$("#nav-toggle").html(" <i class='ionicons ion-navicon-round'></i> ");
+
+
+	} else {
+		//Open menu
+
+		bMenuToggle = true;
+		$( "#navbar" ).addClass("nav-full");
+		$('body').on('scroll mousewheel touchmove', stopScrolling);
+
+		$("#nav-toggle").html(" <i class='ionicons ion-close'></i> ");
+	}
+});
+
+function stopScrolling (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
+}
